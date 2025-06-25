2dcb85ed8ab69996229dda0e0ea4862072ae3f5a
amireyal5 <amirher@gmail.com>
2025-06-25 22:37:59 +0300

    Update App.jsx

--- apps/manager-app/src/App.jsx
+++ apps/manager-app/src/App.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './context/AuthContext';
 
-// ודא שכל הרכיבים האלה מיובאים
 import AuthPage from './components/AuthPage';
 import ManagerDashboard from './components/ManagerDashboard';
 import QuestionnaireDetail from './components/QuestionnaireDetail';
 import TemplateList from './components/TemplateList';
 import FormBuilder from './components/FormBuilder';
-import ExceptionManager from './components/ExceptionManager'; // <-- ייבוא חשוב
+import ExceptionManager from './components/ExceptionManager';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -33,7 +32,6 @@ function AppContent() {
       {currentUser && <Navbar />}
       <div className="container">
         <Routes>
-          {/* אם המשתמש לא מחובר */}
           {!currentUser && (
             <>
               <Route path="/auth" element={<AuthPage />} />
@@ -41,14 +39,12 @@ function AppContent() {
             </>
           )}
 
-          {/* אם המשתמש מחובר */}
           {currentUser && (
             <>
               <Route path="/" element={<ManagerDashboard />} />
               <Route path="/templates" element={<TemplateList />} />
               <Route path="/form-builder/:templateId" element={<FormBuilder />} />
               <Route path="/questionnaire/:id" element={<QuestionnaireDetail />} />
-              {/* --- הנתיב החשוב לטיפול בחריגות --- */}
               <Route path="/exceptions" element={<ExceptionManager />} />
               <Route path="*" element={<Navigate to="/" replace />} />
             </>
